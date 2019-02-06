7307bcb9d3894dfeb00d6d3f64e954cdb890c567
Baek Gi Chan <11618nathan@gmail.com>
2019-02-06 13:38:53 +0900

    Upload

---
+++ computer_system_01/computer_system_01.ino
@@ -0,0 +1,31 @@
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int rled = 13;
+int yled = 12;
+int bled = 11;
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(rled, OUTPUT);
+  pinMode(yled, OUTPUT);
+  pinMode(bled, OUTPUT);
+}
+
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(rled, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(rled, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+  digitalWrite(yled, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(yled, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+  digitalWrite(bled, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(bled, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+}
