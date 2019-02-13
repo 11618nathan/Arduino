659b96dc01b00d98d1adfaae65e7b6cc3c81600e
Baek Gi Chan <11618nathan@gmail.com>
2019-02-13 23:51:58 +0900

    Upload

---
+++ sketch_may16b/sketch_may16b.ino
@@ -0,0 +1,23 @@
+const int motorPin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0; i<256; i++)
+    {
+        analogWrite(motorPin, i);
+        delay(10);
+    }
+    delay(2000);
+    for(int i=255; i>=0; i--)
+    {
+        analogWrite(motorPin, i);
+        delay(10);
+    }
+    delay(2000);
+
+}
